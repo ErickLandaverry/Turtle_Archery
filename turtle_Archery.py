ea0c3b7444b7a49a17cb0e744c53720f6c6e1a78
landy <landy.erick20@gmail.com>
2019-02-05 19:29:30 -0700

    initial commit

---
+++ turtle_Archery.py
@@ -0,0 +1,43 @@
+import turtle
+
+window = turtle.Screen()
+jon = turtle.Turtle()
+
+turtle.bgcolor("gray")
+
+jon.color("white")
+jon.begin_fill()
+jon.circle(125)
+jon.end_fill()
+
+jon.left(90)
+jon.forward(25)
+jon.right(90)
+jon.color("black")
+jon.begin_fill()
+jon.circle(100)
+jon.end_fill()
+
+jon.left(90)
+jon.forward(25)
+jon.right(90)
+jon.color("blue")
+jon.begin_fill()
+jon.circle(75)
+jon.end_fill()
+
+jon.left(90)
+jon.forward(25)
+jon.right(90)
+jon.color("red")
+jon.begin_fill()
+jon.circle(50)
+jon.end_fill()
+
+jon.left(90)
+jon.forward(25)
+jon.right(90)
+jon.color("yellow")
+jon.begin_fill()
+jon.circle(25)
+jon.end_fill()
